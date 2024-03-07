f6b8201c431dd9170fe6f9482b7cabe87e366dcb
John Oh <johnoh0317@gmail.com>
2024-03-07 14:27:45 -0800

    added js for map api

--- scripts/explore.js
+++ scripts/explore.js
@@ -1,38 +1,22 @@
-async function initMap() {
-    const contentString = `
-      <div>
-        <h1>Uluru</h1>
-        <div>
-          <p>
-            <b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large
-            sandstone rock formation in the southern part of the
-            Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)
-            south west of the nearest large town, Alice Springs; 450&#160;km
-            (280&#160;mi) by road. Kata Tjuta and Uluru are the two major
-            features of the Uluru - Kata Tjuta National Park. Uluru is
-            sacred to the Pitjantjatjara and Yankunytjatjara, the
-            Aboriginal people of the area. It has many springs, waterholes,
-            rock caves and ancient paintings. Uluru is listed as a World
-            Heritage Site.
-          </p>
-          <p>
-            Attribution: Uluru,
-            <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
-              https://en.wikipedia.org/w/index.php?title=Uluru
-            </a>
-            (last visited June 22, 2009).
-          </p>
-        </div>
-      </div>`;
-    const infoWindow = new google.maps.InfoWindow({
-      content: contentString,
-      ariaLabel: "Uluru",
-    });
-  
-    const marker = document.querySelector('gmp-advanced-marker');
-    marker.addEventListener('gmp-click', () => {
-      infoWindow.open({anchor: marker});
-    });
-  }
-  
-  window.initMap = initMap;
+const ACCESS_TOKEN = 'pk.eyJ1IjoiaXRzbWVqb2hub2giLCJhIjoiY2x0Z2F1MWpzMHprdTJsc2RpaTJ6b3l3dSJ9.5Ny3ulHEbHGmH7TecVua5w';
+
+mapboxgl.accessToken = ACCESS_TOKEN;
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-73.99209, 40.68933],
+  zoom: 8.8
+});
+
+const searchJS = document.getElementById('search-js');
+searchJS.onload = function () {
+  const searchBox = new MapboxSearchBox();
+  searchBox.accessToken = ACCESS_TOKEN;
+  searchBox.options = {
+    types: 'address,poi',
+    proximity: [-73.99209, 40.68933]
+  };
+  searchBox.marker = true;
+  searchBox.mapboxgl = mapboxgl;
+  map.addControl(searchBox);
+};
