6bc61f211e2005a93e46368dd15785f602428976
DomTorres <neildominictorres@gmail.com>
2024-04-09 06:39:23 -0700

    fixed navbar not loading

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,45 +1,6 @@
 function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
-            // User is signed in.
-            console.log("Logged in.");
-
-            $('#navbarPlaceholder').load('./text/nav_after_login.html');
-            console.log("Logged-in navbar loaded.");
-
-            // $('#footerPlaceholder').load('./text/footer.html');
-            // console.log("Logged-in footer loaded.");
-        } else {
-            // No user is signed in.
-            console.log("Not logged in.");
-
-            $('#navbarPlaceholder').load('./text/nav_before_login.html');
-            console.log("Logged-out navbar loaded.");
-
-            // $('#footerPlaceholder').load('./text/footer.html');
-            // console.log("Logged-out footer loaded.");
-        }
-    });
-}
-// loadSkeleton(); 
-
-function loadMobileSkeleton() {
-    console.log("mobile view");
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log("logged in");
-            $("#footerPlaceholder").load("./text/nav_after_login.html");
-        } else {
-            console.log("not logged in");
-            $("#footerPlaceholder").load("./text/nav_before_login.html");
-        }
-    });
-}
-
-function loadWebSkeleton() {
-    console.log("web view");
-    firebase.auth().onAuthStateChanged(user => {
         if (user) {                
             // User is signed in.
             console.log("Logged in.");
@@ -61,6 +22,45 @@ function loadWebSkeleton() {
         }
     });
 }
+loadSkeleton(); 
+
+// function loadMobileSkeleton() {
+//     console.log("mobile view");
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             console.log("logged in");
+//             $("#footerPlaceholder").load("./text/nav_after_login.html");
+//         } else {
+//             console.log("not logged in");
+//             $("#footerPlaceholder").load("./text/nav_before_login.html");
+//         }
+//     });
+// }
+
+// function loadWebSkeleton() {
+//     console.log("web view");
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {                
+//             // User is signed in.
+//             console.log("Logged in.");
+
+//             $('#navbarPlaceholder').load('./text/nav_after_login.html');
+//             console.log("Logged-in navbar loaded.");
+
+//             // $('#footerPlaceholder').load('./text/footer.html');
+//             // console.log("Logged-in footer loaded.");
+//         } else {
+//             // No user is signed in.
+//             console.log("Not logged in.");
+
+//             $('#navbarPlaceholder').load('./text/nav_before_login.html');
+//             console.log("Logged-out navbar loaded.");
+
+//             // $('#footerPlaceholder').load('./text/footer.html');
+//             // console.log("Logged-out footer loaded.");
+//         }
+//     });
+// }
 
 var userID;
 var userRef;
@@ -99,20 +99,20 @@ function loadUserStats() {
 
 // Media Queries, support for mobile
 
-var query = window.matchMedia("(max-width: 450px)"); 
+// var query = window.matchMedia("(max-width: 450px)"); 
 
-function myFunction(query) {
-    if (query.matches) {
-        // mobile view
-        // alert("mobile view");
-        loadMobileSkeleton();
-    } else {
-        // web view
-        // alert("web view");
-        loadWebSkeleton();
-    }
-  }
-myFunction(query);
+// function myFunction(query) {
+//     if (query.matches) {
+//         // mobile view
+//         // alert("mobile view");
+//         loadMobileSkeleton();
+//     } else {
+//         // web view
+//         // alert("web view");
+//         loadWebSkeleton();
+//     }
+//   }
+// myFunction(query);
 
 // query.addEventListener("change", () => {
 //     myFunction(query);
